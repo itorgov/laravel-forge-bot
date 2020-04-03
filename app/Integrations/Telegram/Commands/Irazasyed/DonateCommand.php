cb4e987102789a746b504ce6b77275028d79ab68
Ivan Torgov <likecod@gmail.com>
2020-04-03 20:25:02 +0300

    Code style fixes.

--- app/Integrations/Telegram/Commands/Irazasyed/DonateCommand.php
+++ app/Integrations/Telegram/Commands/Irazasyed/DonateCommand.php
@@ -24,8 +24,8 @@ class DonateCommand extends Command
         $this->replyWithMessage([
             'text' => 'If you like this bot and find it useful, please give a star to '.
                 '[the GitHub repository](https://github.com/itorgov/laravel-forge-bot). '.
-                'Of course, if you would like to support it, please consider a donation ' .
-                'using my [PayPal](https://paypal.me/WiDe) page. Your donation would help ' .
+                'Of course, if you would like to support it, please consider a donation '.
+                'using my [PayPal](https://paypal.me/WiDe) page. Your donation would help '.
                 '[me](https://t.me/ivantorgov) a lot to continue running this bot and covering hosting costs.',
             'parse_mode' => 'Markdown',
         ]);
